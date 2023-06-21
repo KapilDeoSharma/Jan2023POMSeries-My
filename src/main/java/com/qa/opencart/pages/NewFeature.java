09c39a28497dd6edca3ad5124e9186e4ccd11225
KapilDeoSharma <89604957+KapilDeoSharma@users.noreply.github.com>
2023-06-21 00:23:38 -0500

    Update NewFeature.java

--- src/main/java/com/qa/opencart/pages/NewFeature.java
+++ src/main/java/com/qa/opencart/pages/NewFeature.java
@@ -5,4 +5,5 @@ public class NewFeature {
 	/*****Just a test file****/
 	
 	String soMuch = "Delete when you want";
+	/****** Team member made changes and already puched code in Remote Master********/
 }
